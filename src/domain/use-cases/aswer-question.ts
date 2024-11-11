fee9a2fa340b722355ce8fb7d0cb0e310555f506
EnzoMoita <enzosadourado@hotmail.com>
2024-11-10 21:56:57 -0300

    feat: firt use case and firt test with Vitest

--- src/domain/use-cases/aswer-question.ts
+++ src/domain/use-cases/aswer-question.ts
@@ -1,17 +1,19 @@
+import { Answer } from "../entities/answer";
+
+
 interface AnswerQuestionUseCaseRequest {
     instructorId: string;
     questionId: string;
+    content: string;
 
 }
 
 
 export class AnswerQuestionUseCase {
-    execute({ instructorId, questionId }: AnswerQuestionUseCaseRequest): void {
+    execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+        const answer = new Answer (content)
+
+        return answer
 
     }
 }
-
-new AnswerQuestionUseCase().execute({
-    questionId: '1',
-    instructorId: '2',
-})
