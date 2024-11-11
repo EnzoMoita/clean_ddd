cbb70a80651f06215eb66aa46da58123d7a0590b
EnzoMoita <enzosadourado@hotmail.com>
2024-11-10 23:56:37 -0300

    feat: new format in domains files

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,12 +1,22 @@
 import { randomUUID } from "node:crypto"
 
+interface AnswerDataProps {
+    content: string,
+     authorId: string, 
+     questionId: string, 
+}
+
 export class Answer {
     public content: string
     public id: string
+    public authorId: string
+    public questionId: string
 
-    constructor( content: string, id?: string) {
-        this.content = content
+    constructor( props: AnswerDataProps, id?: string) {
+        this.content = props.content
         this.id = id ?? randomUUID()
+        this.authorId = props.authorId
+        this.questionId = props.questionId
     }
 
 }
