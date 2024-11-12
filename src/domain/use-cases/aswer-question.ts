8685e99eda8388638ee3a74aaed1652f0af02660
EnzoMoita <enzosadourado@hotmail.com>
2024-11-11 23:01:21 -0300

    feat: create external dependence

--- src/domain/use-cases/aswer-question.ts
+++ src/domain/use-cases/aswer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from "../entities/answer";
+import { AnswerRepository } from "../repositories/answers-repository";
 
 
 interface AnswerQuestionUseCaseRequest {
@@ -10,12 +11,18 @@ interface AnswerQuestionUseCaseRequest {
 
 
 export class AnswerQuestionUseCase {
-    execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    constructor(
+        private answersRepository: AnswerRepository,
+    ){}
+
+  async  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
         const answer = new Answer ({
             content,
             authorId: instructorId,
             questionId,
         })
+
+        await this.answersRepository.create(answer)
 
         return answer
 
