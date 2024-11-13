45c24c3478dd44f473f13911d6562e2b5c9e04b6
EnzoMoita <enzosadourado@hotmail.com>
2024-11-12 23:17:57 -0300

    fix type for change

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -4,12 +4,14 @@ interface QuestionDataProps {
     title: string,
      content: string, 
      authorId: string,
+     slug: string
 }
 
 export class Question {
     public title: string
     public content: string
     public id: string
+    public slug: string
     public authorId: string
 
     constructor( props: QuestionDataProps, id?: string,) {
@@ -17,6 +19,7 @@ export class Question {
         this.content = props.content
         this.authorId = props.authorId
         this.id = id ?? randomUUID()
+        this.slug = props.slug
 
     }
 }
